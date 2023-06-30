0e73a46187cc2ffbbbfd9491cbe6eef4a6f8b8a7
Kristian Hannula <kristian.hannula@proton.me>
2023-06-30 16:50:26 +0300

    Task 6.20 done, anecdotes are fetched from server with query

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
+import { useQuery } from 'react-query'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { getAnecdotes } from './requests'
 
 const App = () => {
-
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
+  const result = useQuery(
+    'anecdotes', getAnecdotes,
     {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+      retry: 1
+    }
+  )
+
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+
+  if (result.isError) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
       <h3>Anecdote app</h3>
-    
+
       <Notification />
       <AnecdoteForm />
-    
+
       {anecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
